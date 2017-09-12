5a983b928566457e13c5113abbc55aca68e0b56a
nishankpriya-jain <nishankpriya.jain@gmail.com>
2017-09-12 12:11:17 +0530

    training done on multiples files and summarized to a single combo_tree

--- markov_chain.cpp
+++ markov_chain.cpp
@@ -39,7 +39,7 @@ string int2str(int x){
 
 long long int str2num(string s){
    long long int x=0;
-   for(int i=(int)s.size()-1;i>=0;i--){
+   for(int i=0;i<s.size();i++){
         x=(x*10)+s[i]-'0';
    }
    
@@ -63,6 +63,25 @@ void printChains(string tmp,struct node* now){
 }
 
 
+void printTree(string tmp,struct node * now,string file_name){
+     int f=0;
+     for(int i=0;i<300;i++){
+         if(now->link[i]!=NULL){
+            f++;
+            printTree(tmp+(char)i,now->link[i],file_name);
+         }
+     }
+     if(!f){
+        ofstream out;
+        out.open(file_name.c_str(),ofstream::app);
+        //cout<<tmp<<" "<<now->ct<<endl;
+        out<<tmp<<" "<<now->ct<<endl;
+        out.close();
+     }
+     
+     return ;
+}
+
 string createDummy(int order){
    string tmp="";
    for(int i=0;i<order;i++){
@@ -71,23 +90,33 @@ string createDummy(int order){
   return tmp;
 }
 
-double query(string s,int sz){
+void deleteTree(struct node * now){
+    for(int i=0;i<300;i++){
+         if(now->link[i]!=NULL){
+            deleteTree(now->link[i]);
+         }
+    }
+    free(now);    
+}
+
+double query(struct node* now,string s,int sz){
     
-    struct node* tmp=root;
+    struct node* tmp=now;
     double num,deno;
-    cout<<s<<" sz="<<sz<<endl;
+    cout<<"in query "<<s<<" sz="<<sz<<endl;
     for(int i=0;i<sz-1;i++){
+       cout<<"char now="<<s[i]<<" ";
        tmp=tmp->link[(int)s[i]];
     } 
     deno=tmp->ct;
     tmp=tmp->link[(int)s[sz-1]];
     num=tmp->ct;
-    
+    cout<<endl;
     cout<<"char= "<<s[sz-1]<<" ascii="<<(int)s[sz-1]<<" num="<<num<<" deno="<<deno<<" prob="<<(num/deno)<<endl;
     return (num/deno);    
 }
 
-void testPass(int order){
+void testPass(struct node* now,int order){
 	string in_pass,dummy,tmp,window;
 	cout<<"password: ";
 	cin>>in_pass;
@@ -100,7 +129,7 @@ void testPass(int order){
 	tmp=dummy+in_pass;
 	for(int pos=0;pos<=(int)tmp.size()-window_size;pos++){
 	    window=tmp.substr(pos,window_size);
-            tmp_prob=query(window,window_size);         
+            tmp_prob=query(now,window,window_size);         
 	    prob_char.push_back(tmp_prob);
 	    total_prob*=tmp_prob;
 	}		
@@ -118,13 +147,12 @@ void  insertNgram(struct node* now,string s,int sz,long long int wt){
         }
        tmp=tmp->link[(int)s[i]];
        tmp->ct = tmp->ct +wt;
-       
     }
     
 }
 
 void  insertNgram(struct node* now,string s,int sz,long long int wt1,long long int wt2){
-    cout<<"inserting "<<s<<" "<<wt1<<" "<<wt2<<endl;
+    //cout<<"inserting "<<s<<" "<<wt1<<" "<<wt2<<endl;
     struct node* tmp=now;
     for(int i=0;i<sz;i++){
         if(tmp->link[(int)s[i]]==NULL){
@@ -137,7 +165,7 @@ void  insertNgram(struct node* now,string s,int sz,long long int wt1,long long i
        else{
        		tmp->ct = tmp->ct +wt2;
        }
-       cout<<"s[i]="<<s[i]<<" ct="<<tmp->ct<<endl;
+      // cout<<"s[i]="<<s[i]<<" ct="<<tmp->ct<<endl;
     }
     
 }
@@ -147,69 +175,58 @@ void  insertNgram(struct node* now,string s,int sz,long long int wt1,long long i
 void inputTrain(int order){
     //input the strings from the target files and insert the N-grams in the model.
      ifstream input;
-     string file_name,pass,tmp,window,dummy;
+     string file_name,pass,tmp,r1,r2,window,dummy;
      dummy=createDummy(order);
      int window_size=order+1;
-     FILE *fp;
-     
-     for(int fno=0;fno<1;fno++){
-       //file_name="input"+int2str(fno);
-       file_name="input/dummy.txt";
+     long long int total_ct=0,tmp_ct=0,wt;      
+     for(int fno=0;fno<20;fno++){
+        if(fno==10 || fno==12 || fno==18)continue;
+       file_name="input/input"+int2str(fno)+".txt";
        input.open(file_name.c_str(),ifstream::in);
-       //fp=fopen(file_name.c_str(),"r");
-        int tmp_ct=0;
-         //while(tmp_ct<3){
-          while( input>>pass ){
-         // while(fscanf(fp,"%s",pass.c_str()) ){
-                 //cin >> pass;
-                 cout<<pass<<endl;
+       tmp_ct=0;
+          while( input>>r1 ){
+                  
+                 if(fno>17){
+                     input>>r2;
+                     pass=r2;
+                     wt=str2num(r1);   
+                 }
+                 else{
+                     pass=r1;
+                     wt=1LL;	  
+                 }
+                 
+                 cout<<pass<<"wt= "<<wt<< " filename="<<file_name<<endl;
                  pass=dummy+pass;
                  for(int pos=0;pos <=(int)pass.size()-window_size;pos++){
                      window=pass.substr(pos,window_size);
-                     insertNgram(root,window,window_size,1LL);
+                     insertNgram(root,window,window_size,wt);
                      //cout<<"window="<<window<<endl;
                  }
-               tmp_ct++; 
+               tmp_ct++;
+               total_ct++; 
                cout<<tmp_ct<<endl; 
          }
        input.close();
-       //fclose(fp);  
-      cout<<"tmp_ct= "<<tmp_ct<<endl;
+       printTree("",root,"output/tree.txt");
+       deleteTree(root);  
+       root=makeNode();
+       cout<<"tmp_ct= "<<tmp_ct<<endl;
      }
+     cout<<"total_ct= "<<total_ct<<endl;
 
 }
 
-void printTree(string tmp,struct node * now,string file_name){
-     int f=0;
-     for(int i=0;i<300;i++){
-         if(now->link[i]!=NULL){
-            f++;
-            printTree(tmp+(char)i,now->link[i],file_name);
-         }
-     }
-     if(!f){
-        ofstream out;
-        out.open(file_name.c_str(),ofstream::app);
-        cout<<tmp<<" "<<now->ct<<endl;
-        out<<tmp<<" "<<now->ct<<endl;
-        out.close();
-     }
-     
-     return ;
 
-}
-
-void trainFromTree(){
+void trainFromTree(string file_name){
 	ifstream in;
-	in.open("output/tree.txt",ifstream::in);
+	in.open(file_name.c_str(),ifstream::in);
 	string s;
 	long long int wt;
 	while(in>>s){
 	   in>>wt;
 	   insertNgram(combo_root,s,s.size(),wt);
 	}
-		
-
 	return;
 }
 
@@ -219,17 +236,22 @@ int main(){
 	int order=3;
 	root=makeNode();
 	combo_root=makeNode();
-	inputTrain(order);
-	cout<<"calling prints\n";
+	//cout<<"calling training\n";
+	//inputTrain(order);
+	//cout<<"calling prints\n";
 	//printChains("",root);
 	
-	cout<<"printing to output/tree.txt\n";
-	printTree("",root,"output/tree.txt");
-	cout<<"Training from tree\n";
-	trainFromTree();
-	cout<<"printing to output/combo_tree.txt\n";
-	printTree("",combo_root,"output/combo_tree.txt");
-	//testPass(order);	               
-        
+	//cout<<"printing to output/tree.txt\n";
+	//printTree("",root,"output/tree.txt");
+	//cout<<"Training from tree\n";
+	//trainFromTree("output/tree.txt");
+	//printChains("",combo_root);
+	cout<<"Training from combo_tree\n";
+	trainFromTree("output/combo_tree.txt");
+	
+	//cout<<"printing to output/combo_tree.txt\n";
+	//printTree("",combo_root,"output/combo_tree.txt");
+	for(int i=0;i<100;i++)
+	testPass(combo_root,order);	               
 return 0;
 }
